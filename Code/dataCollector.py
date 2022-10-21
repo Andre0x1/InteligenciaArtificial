022b2a9b980f07b23484511ce99dc7a275d1ec99
Andre0x1 <andrel.silva3357@gmail.com>
2022-10-21 00:40:03 -0300

    Ajustes pastas

--- Code/dataCollector.py
+++ Code/dataCollector.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-Dev_file_path = '..input/devImpactDataSet.csv'
+Dev_file_path = 'Data/devImpactDataSet.csv'
 # read the data and store data in DataFrame titled melbourne_data
 DevImpact = pd.read_csv(Dev_file_path) 
 # print a summary of the data in Melbourne data
